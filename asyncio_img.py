895ceb1db131b19fa4b308f445eebce3f38af869
Varvara <egorova.va@list.ru>
2020-05-10 23:50:40 +0300

    Add doc

--- asyncio_img.py
+++ asyncio_img.py
@@ -4,6 +4,7 @@
 import asyncio
 from io import BytesIO
 import requests
+import aiohttp
 from requests import RequestException
 import logging
 import sys
@@ -40,7 +41,7 @@ def get_img_names() -> list:
     return img_list_names
 
 
-def mk_dir(dir_name=IMG_DIR):
+def mk_dir(dir_name=IMG_DIR) -> None:
     try:
         os.mkdir(dir_name)
         logger.info("Directory %s Created", dir_name) 
@@ -48,35 +49,41 @@ def mk_dir(dir_name=IMG_DIR):
         logger.warning("Directory %s already exists", dir_name)
 
 
-def mirror_img(b_img_data: bytes):
+def mirror_img(b_img_data: bytes) -> Image:
     imageStream = BytesIO(b_img_data)
     img = Image.open(imageStream)
     mirror_image = ImageOps.mirror(img)
     return mirror_image
 
 
-def post_img(image, image_file_path):
+def post_img(image, image_file_path: str, session) -> None:
     url = HOST_ADDR
-    files = {'image': open(image_file_path, 'rb')}
-    requests.post(url, files=files)
+    b_image = open(image_file_path, 'rb')
+    files = {'image': b_image}
+    session.post(url, data=b_image)
 
 
-def dowload_file(file_name: str) -> None:
+async def dowload_file(file_name: str, session) -> None:
     image_url = HOST_ADDR + file_name
     img_data = requests.get(image_url).content
     img = mirror_img(img_data)
 
     image_file_path = IMG_DIR + file_name
     img.save(image_file_path)
-    post_img(img, image_file_path)
+    await post_img(img, image_file_path, session=session)
 
 
-def dowload_transform_send_images(image_names: list) -> None:
-    mk_dir()
-    for img_name in img_names_list:
-        dowload_file(img_name)
+async def dowload_transform_send_images(img_names_list: list) -> None:
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for image_name in img_names_list:
+            tasks.append(
+                dowload_file(image_name, session=session)
+            )
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
+    mk_dir()
     img_names_list = get_img_names()
-    dowload_transform_send_images(img_names_list)
+    asyncio.run(dowload_transform_send_images(img_names_list))
