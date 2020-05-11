2d6f4c614eba3b0d0d671b1d85b19eba7d8bcedb
Varvara <egorova.va@list.ru>
2020-05-11 03:00:12 +0300

    Recreated async/await processing

--- asyncio_img.py
+++ asyncio_img.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # asyncio_img.py
 
+"""Asynchronously get, process and post images."""
 import asyncio
-from io import BytesIO
-import requests
-import aiohttp
-from requests import RequestException
-import logging
 import sys
-import os
+import time
+import logging
+import requests
+from io import BytesIO
 from PIL import Image, ImageOps
+from asyncio import TimeoutError
+from aiohttp import ClientSession, ClientResponseError, \
+                    ClientError, \
+                    ClientOSError, \
+                    ServerTimeoutError
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(name)s: %(message)s',
@@ -19,71 +23,71 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
 HOST_ADDR = 'http://142.93.138.114/images/'
-IMG_DIR = 'images/'
 
-class RequestError(Exception):
-    pass
+def get_img_urls(url: HOST_ADDR) -> list:
+    '''
+        Request list of image names & returns images urls
 
-def get_img_names() -> list:
+        params: host url
+        returns: urls of images (list)
+    '''
     try:
-        response = requests.get(HOST_ADDR)
-    except RequestException:
-        logger.warning("Request exception has occured with %s", HOST_ADDR)
+        response = requests.get(url)
+    except requests.RequestException:
+        logger.warning("Request exception has occured with %s", url)
         return []
 
-    if response.status_code != 200:
-        raise RequestError
-
-    img_list_names = response.text.split()
-    logger.info("Got list of image names from source: %s", HOST_ADDR)
-
+    img_list_names = [url + _ for _ in response.text.split()]
     return img_list_names
 
 
-def mk_dir(dir_name=IMG_DIR) -> None:
-    try:
-        os.mkdir(dir_name)
-        logger.info("Directory %s Created", dir_name) 
-    except FileExistsError:
-        logger.warning("Directory %s already exists", dir_name)
+async def process_img(session: ClientSession, url: str) -> None:
+    '''
+        Request & mirror & upload by POST for multiple images.
+        params:
+            session: created as a context manager ClientSession
+            url: url of image
+    '''
+    async with session.get(url) as response:
+        logger.debug("Read %i from %s with code %i", response.content_length, url, response.status)
+        if response.status == 200:
+            data = await response.read()
+            img = Image.open(BytesIO(data))
+            mirrored_image = ImageOps.mirror(img)
+            byte_data = BytesIO(ImageOps.mirror(img).tobytes())
+            try:
+                async with session.post(HOST_ADDR, data=byte_data) as post_response:
+                    logger.debug("Data sent with status %s", post_response.status)
+            except (ClientResponseError,
+                    ClientError,
+                    ClientOSError,
+                    ServerTimeoutError,
+                    TimeoutError) as e:
+                logger.warning("Exception was occured in POST request %s", e)
+                
 
-
-def mirror_img(b_img_data: bytes) -> Image:
-    imageStream = BytesIO(b_img_data)
-    img = Image.open(imageStream)
-    mirror_image = ImageOps.mirror(img)
-    return mirror_image
-
-
-def post_img(image, image_file_path: str, session) -> None:
-    url = HOST_ADDR
-    b_image = open(image_file_path, 'rb')
-    files = {'image': b_image}
-    session.post(url, data=b_image)
-
-
-async def dowload_file(file_name: str, session) -> None:
-    image_url = HOST_ADDR + file_name
-    img_data = requests.get(image_url).content
-    img = mirror_img(img_data)
-
-    image_file_path = IMG_DIR + file_name
-    img.save(image_file_path)
-    await post_img(img, image_file_path, session=session)
-
-
-async def dowload_transform_send_images(img_names_list: list) -> None:
-    async with aiohttp.ClientSession() as session:
+async def download_all_img(urls: list) -> None:
+    '''
+        Share the session across all tasks. 
+        Session context works until all of the tasks have completed.
+        params:
+            urls: urls of images (list)
+    '''
+    async with ClientSession() as session:
         tasks = []
-        for image_name in img_names_list:
-            tasks.append(
-                dowload_file(image_name, session=session)
-            )
-    await asyncio.gather(*tasks)
+        for url in urls:
+            task = asyncio.ensure_future(process_img(session, url))
+            tasks.append(task)
+        await asyncio.gather(*tasks, return_exceptions=False)
 
 
 if __name__ == "__main__":
-    mk_dir()
-    img_names_list = get_img_names()
-    asyncio.run(dowload_transform_send_images(img_names_list))
+
+    urls = get_img_urls()
+
+    start_time = time.time()
+    asyncio.get_event_loop().run_until_complete(download_all_img(urls))
+    duration = time.time() - start_time
+    print(duration)
