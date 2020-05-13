c5fb1d391111887ef38ca2aa5c4b8ce79329c4b6
Varvara <egorova.va@list.ru>
2020-05-14 02:46:19 +0300

    Fixed comments

--- asyncio_img.py
+++ asyncio_img.py
@@ -7,9 +7,9 @@ import sys
 import time
 import logging
 import requests
-from io import BytesIO
+from io import BytesIO, StringIO
 from PIL import Image, ImageOps
-from asyncio import TimeoutError
+from PIL.JpegImagePlugin import JpegImageFile
 from aiohttp import ClientSession, ClientResponseError, \
                     ClientError, \
                     ClientOSError, \
@@ -27,12 +27,12 @@ logger = logging.getLogger(__name__)
 HOST_ADDR = 'http://142.93.138.114/images/'
 
 def get_img_urls(url=HOST_ADDR) -> list:
-    '''
+    """
         Request list of image names & returns images urls
 
         params: host url
         returns: urls of images (list)
-    '''
+    """
     try:
         response = requests.get(url)
     except requests.RequestException:
@@ -44,50 +44,51 @@ def get_img_urls(url=HOST_ADDR) -> list:
 
 
 async def process_img(session: ClientSession, url: str) -> None:
-    '''
+    """
         Request & mirror & upload by POST for multiple images.
         params:
             session: created as a context manager ClientSession
             url: url of image
-    '''
+    """
     async with session.get(url) as response:
         logger.debug("Read %i from %s with code %i", response.content_length, url, response.status)
         if response.status == 200:
             data = await response.read()
             img = Image.open(BytesIO(data))
-            mirrored_image = ImageOps.mirror(img)
+
             byte_data = BytesIO(ImageOps.mirror(img).tobytes())
+            
             try:
                 async with session.post(HOST_ADDR, data=byte_data) as post_response:
                     logger.debug("Data sent with status %s", post_response.status)
             except (ClientResponseError,
                     ClientError,
-                    ClientOSError,
-                    ServerTimeoutError,
-                    TimeoutError) as e:
+                    ServerTimeoutError) as e:
                 logger.warning("Exception was occured in POST request %s", e)
                 
 
 async def download_all_img(urls: list) -> None:
-    '''
+    """
         Share the session across all tasks. 
         Session context works until all of the tasks have completed.
         params:
             urls: urls of images (list)
-    '''
+    """
     async with ClientSession() as session:
         tasks = []
         for url in urls:
-            task = asyncio.ensure_future(process_img(session, url))
+            task = asyncio.create_task(process_img(session, url))
             tasks.append(task)
         await asyncio.gather(*tasks, return_exceptions=False)
 
 
 if __name__ == "__main__":
 
+    assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
+
     urls = get_img_urls()
 
     start_time = time.time()
-    asyncio.get_event_loop().run_until_complete(download_all_img(urls))
+    asyncio.run(download_all_img(urls))
     duration = time.time() - start_time
     print(duration)
